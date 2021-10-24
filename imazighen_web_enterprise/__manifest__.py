96661dd2f3e941c351465222261591db7b42c9d7
imazighenapps <62152141+imazighenapps@users.noreply.github.com>
2021-10-24 15:05:11 +0100

    Add files via upload

---
+++ imazighen_web_enterprise/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Imazighen Web Enterprise',
+    "category": "Theme/Backend",
+    "version": "14.0.1.0.0",
+    'author': 'Imazighen',
+    'currency': 'EUR',
+    'price': 75.0,
+    'depends': ['web'],
+    
+    'description': """This module provide Enterprise design and responsiveness.
+        """,
+
+    'depends': ['web'],
+    'images': ['static/description/home.gif'],
+    'auto_install': False,
+    'data': [
+        
+    ],
+    'assets': {
+       
+    },
+    'license': 'OPL-1',
+}
