78159821591c3554351ded1d0ace5aa748a1cf0e
imazighenapps <62152141+imazighenapps@users.noreply.github.com>
2022-06-02 09:56:22 +0100

    Update __manifest__.py

--- tree_direct_form_wizard/__manifest__.py
+++ tree_direct_form_wizard/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Tree Direct Form Wizard',
     'version': '1.0.0',
-    'price': 29.0,
+    'price': 5.0,
     'currency': 'EUR',
     'license': 'OPL-1',
     'summary': """Display the wizard form view on hovering a row in the tree view""",
