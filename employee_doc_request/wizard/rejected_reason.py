64ecd0e1daec2a4585e45ec113d61b5f0f07777d
imazighenapps <62152141+imazighenapps@users.noreply.github.com>
2022-09-29 11:15:49 +0100

    Add files via upload

---
+++ employee_doc_request/wizard/rejected_reason.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields,api
+
+class RejectedReason(models.TransientModel):
+    _name = 'rejected.reason'
+
+    name  = fields.Char('Reason')
+
+
+    def do_reject(self):
+        line_val = {"request_id"    : self.env.context.get('active_id'),
+                    "state"         :"rejected",   
+                    "employee_id"   : self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1).id,
+                    "date"          : fields.Date.today(),
+                    "reason"        :self.name,
+        }
+      
+        self.env['doc.request'].search([('id','=',self.env.context.get('active_id'))]).write({
+                                                                                    'state':'rejected',
+                                                                                    'rejected_reason': self.name,
+                                                                                    "rejected_date"  : fields.Date.today(),
+                                                                                    }) 
+
+
---
+++ employee_material_management/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+{
+    
+    'name': 'Employee Material Acquisition Management',
+    'version': '1.3.3',
+    'price': 15.0,
+    'currency': 'EUR',
+    'license': 'OPL-1',
+    'summary': """This module allow your employees to request material acquisition""",
+    'description': """  """,
+    'author': 'Imazighen',
+    'website': '',
+    'support': 'imazighenapps@gmail.com',
+    'images': ['static/description/c_image.png'],
+ 
+    'category': 'Human Resources',
+    'depends': ['hr','stock','purchase'],
+    'data':[
+        'security/security.xml',
+        'security/multi_company_security.xml',
+        'security/ir.model.access.csv',
+        'data/material_request_sequence.xml',
+        'views/material_request.xml',
+        'wizard/rejected_reason.xml',
+        'wizard/create_po.xml',
+        'wizard/create_picking.xml',
+        'menu/menu.xml',
+    ],
+    'installable' : True,
+    'application' : False,
+}
+
