1e71a659741a16e67d7f4cd523595abc7b157f6f
imazighenapps <62152141+imazighenapps@users.noreply.github.com>
2022-10-24 11:39:07 +0100

    Add files via upload

---
+++ im_server_monitoring/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "IM Server Monitoring",
+
+    'summary': """ """,
+
+    'description': """
+        
+    """,
+
+    'author': "Imazighen",
+    'website': "",
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'price': 10.0,
+    'category': 'Tools',
+    'version': '1.0',
+
+    'depends': ['base'],
+    'images': ['static/description/cpu_icon.PNG'],
+    # always loaded
+    'data': [
+        
+        'security/res_groups.xml',
+        'security/ir.model.access.csv',
+        'views/assets.xml',
+        'views/server_monitoring.xml',
+       
+      
+        
+       
+        'menu/menu.xml',
+       
+  
+    ],
+
+    'qweb': ['static/src/xml/template.xml'],
+  
+}
