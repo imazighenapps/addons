5af4627de96dcbc15d1770fe07a23c378a293171
Farid SLIMANI <62152141+imazighenapps@users.noreply.github.com>
2022-12-25 11:32:42 +0100

    Add files via upload

---
+++ invoice_search_panel/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Invoice Search Panel",
+    "summary": "The module add the search panel in Invoice",
+    "version": "15.0",
+    'price': 10.0,
+    'support': 'imazighenapps@gmail.com',
+    'currency': 'EUR',
+    'images': ['static/description/img.PNG'],
+    "category": "sale",
+    "website": "",
+    "author": "Farid SLIMANI",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["base","account"],
+    "data": [
+             'views/account_move.xml',
+         
+         ],
+
+
+    
+    "sequence": 1,
+}
