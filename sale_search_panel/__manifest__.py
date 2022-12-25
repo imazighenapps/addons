713e33748c4628bee33974893480146213233413
Farid SLIMANI <62152141+imazighenapps@users.noreply.github.com>
2022-12-25 11:12:08 +0100

    Add files via upload

---
+++ sale_search_panel/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Sale Search Panel",
+    "summary": "The module add the search panel in Sales Orders and Quotations",
+    "version": "16.0",
+    'price': 10.0,
+    'support': 'imazighenapps@gmail.com',
+    'currency': 'EUR',
+    'images': ['static/description/img.PNG'],
+    "category": "sale",
+    "website": "",
+    "author": "Farid SLIMANI",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["base","sale_management"],
+    "data": [
+             'views/sale_order.xml',
+         
+         ],
+
+
+    
+    "sequence": 1,
+}
