3e7f1f6a510f0e167c6c0ebd2380e52c2e660318
Farid SLIMANI <62152141+imazighenapps@users.noreply.github.com>
2023-09-14 13:44:31 +0100

    Add files via upload

--- fs_dashboard/__manifest__.py
+++ fs_dashboard/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Farid SLIMANI",
     "license": "LGPL-3",
     "installable": True,
-    'images': ['static/description/appointment.jpg'],
+    'images': ['static/description/img/dash.jpg'],
     "depends": ["base",],
     "data": [
              'security/ir.model.access.csv',
