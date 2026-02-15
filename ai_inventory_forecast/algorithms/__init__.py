a0df6ccaccccec66783f77322d45cd4ccdefa3c8
Farid SLIMANI <62152141+imazighenapps@users.noreply.github.com>
2026-02-15 14:09:03 +0100

    Delete ai_inventory_forecast directory

--- ai_inventory_forecast/algorithms/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import forecast_engine
-from . import prophet_forecaster
-from . import arima_forecaster
-from . import ensemble_forecaster
--- ai_inventory_forecast/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import forecast_config
-from . import forecast_prediction
-from . import forecast_alert
-from . import forecast_reorder
-from . import product_product
